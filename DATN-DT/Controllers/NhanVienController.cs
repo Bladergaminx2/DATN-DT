338f8daba6884910db16e485314fe2b9bc6292b5
lnl9904 <lenholoc132@gmail.com>
2026-01-05 21:59:36 +0700

    revert

--- DATN-DT/Controllers/NhanVienController.cs
+++ DATN-DT/Controllers/NhanVienController.cs
@@ -19,8 +19,6 @@ namespace DATN_DT.Controllers
         public async Task<IActionResult> Index()
         {
             var nhanVienList = await _nhanVienService.GetAllNhanViens();
-            var chucVus = await _nhanVienService.GetChucVusExceptAdmin();
-            ViewBag.ChucVus = chucVus;
             return View(nhanVienList);
         }
 
